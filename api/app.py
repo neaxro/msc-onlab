3fc4f5ad8f9c8b9debb87a045a93f24c374b839c
Axel Roland Nemes <neaxro@gmail.com>
2024-03-11 20:40:03 +0100

    Added invitation to endpoint

--- api/app.py
+++ api/app.py
@@ -9,6 +9,8 @@ from app.user.user_routes import UserResource
 
 from app.household.household_routes import HouseholdResource
 from app.household.household_detailed_routes import HouseholdDetailedResource
+from app.household.invitation.invitation_create_routes import InvitationCreateResource
+from app.household.invitation.invitation_accept_routes import InvitationAcceptResource
 
 from app.task.task_routes import TaskResource
 from app.task.task_detailed_routes import TaskDetailedResource
@@ -23,6 +25,9 @@ api.add_resource(UserResource, '/user', '/user/<id>')
 # Hosuehold resources
 api.add_resource(HouseholdResource, '/household', '/household/all')
 api.add_resource(HouseholdDetailedResource, '/household', '/household/all/detailed', '/household/id/<household_id>')
+api.add_resource(InvitationCreateResource, '/household/invite/')
+api.add_resource(InvitationAcceptResource, '/household/accept-invite/<invitation_token>')
+
 
 # Task resources
 api.add_resource(TaskResource, '/task', '/task/all/household/<household_id>', '/task/add-to/<household_id>')
