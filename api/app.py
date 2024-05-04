117d5560d30fa60ec78ddd32c33d14d6fc4dcc11
Axel Roland Nemes <neaxro@gmail.com>
2024-05-04 10:41:04 +0200

    Implement subtask related functions

--- api/app.py
+++ api/app.py
@@ -19,6 +19,8 @@ from app.household.tasks.task_unassign_routes import UnassignResource
 from app.household.tasks.task_routes import TaskResource
 from app.household.tasks.task_detailed_routes import TaskDetailedResource
 
+from app.household.subtasks.subtask_routes import SubtaskResource
+
 # Auth resources
 api.add_resource(LoginResource, '/auth/login')
 api.add_resource(RegisterResource, '/auth/register')
@@ -38,6 +40,9 @@ api.add_resource(TaskResource, '/household', '/household/id/<household_id>/tasks
 api.add_resource(TaskDetailedResource, '/household', '/household/id/<household_id>/tasks/all/detailed', '/household/tasks/id/<task_id>/detailed')
 api.add_resource(AssignResource, '/tasks/id/<task_id>/assign-to/<user_id>')
 api.add_resource(UnassignResource, '/tasks/id/<task_id>/unassign-user')
+
+# Subtask resources
+api.add_resource(SubtaskResource, '/subtask/<subtask_id>', '/subtask/add-to/<task_id>', '/subtask/<subtask_id>/remove-from/<task_id>', '/subtask/<subtask_id>/for/<task_id>')
 
 if __name__ == '__main__':
     create_env_variables()
