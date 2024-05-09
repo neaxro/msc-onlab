cc59d318a151bc126fba0a03442ffeefd3b3a61b
Axel Roland Nemes <neaxro@gmail.com>
2024-05-09 18:28:30 +0200

    Remove old files

--- api/test/test_login.py
+++ api/test/test_login.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_restful import Api
 import pytest
 
+from test.environment.database import MockedDatabase
+
+from app.db.app_database import AppDatabase
+from app.user.user_service import UserService
 from app.utils.time_management import utcnow
 
 @pytest.fixture()
@@ -17,6 +21,10 @@ def app():
     yield app
 
     # clean up / reset resources here
+
+@pytest.fixture()
+def database() -> AppDatabase:
+    return MockedDatabase()
 
 @pytest.fixture()
 def client(app):
@@ -42,3 +50,17 @@ def test_json_validation(client):
     
     date = utcnow()
     assert date is not None
+
+def test_user_service(database):
+    user_service = UserService(database)
+    
+    user = user_service.get_user_by_id("65dcf20484273f26b75734f0")
+    
+    assert user is not None
+
+def test_user_service(database):
+    user_service = UserService(database)
+    
+    user = user_service.get_user_by_id("65dcf20484273f26b75734f9")
+    
+    assert user is None
