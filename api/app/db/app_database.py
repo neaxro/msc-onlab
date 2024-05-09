e38ad3cd95a20723955625b725b6198e2acd836b
Axel Roland Nemes <neaxro@gmail.com>
2024-05-09 18:28:59 +0200

    Fix database classes

--- api/app/db/app_database.py
+++ api/app/db/app_database.py
@@ -1,4 +1,26 @@
 from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
+from abc import ABC, abstractmethod
 
-class AppDatabase:
-    pass
+class AppDatabase(ABC):
+    
+    @property
+    @abstractmethod
+    def client(self) -> MongoClient:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def db(self) -> Database:
+        raise NotImplementedError
+    
+    @property
+    @abstractmethod
+    def user_collection(self) -> Collection:
+        raise NotImplementedError
+    
+    @property
+    @abstractmethod
+    def household_collection(self) -> Collection:
+        raise NotImplementedError
--- api/app/db/mongo.py
+++ api/app/db/mongo.py
@@ -1,16 +1,34 @@
 import os
 from app.db.app_database import AppDatabase
 from pymongo import MongoClient
+from pymongo.database import Database
+from pymongo.collection import Collection
 
 
 class MongoDatabase(AppDatabase):
     def __init__(self):
-        mongodb_url = os.getenv("MONGODB_CONNECTION_URL")
-        db_name = os.getenv("MONGODB_DATABASE_NAME")
-        user_collection_name = os.getenv("MONGODB_COLLECTION_USERS")
-        household_collection_name = os.getenv("MONGODB_COLLECTION_HOUSEHOLDS")
+        self.mongodb_url = os.getenv("MONGODB_CONNECTION_URL")
+        self.db_name = os.getenv("MONGODB_DATABASE_NAME")
+        self.user_collection_name = os.getenv("MONGODB_COLLECTION_USERS")
+        self.household_collection_name = os.getenv("MONGODB_COLLECTION_HOUSEHOLDS")
         
-        self.client = MongoClient(mongodb_url)
-        self.db = self.client[db_name]
-        self.user_collection = self.db[user_collection_name]
-        self.household_collection = self.db[household_collection_name]
+        # self.client: MongoClient = MongoClient(mongodb_url)
+        # self.db: MongoDatabase = self.client[db_name]
+        # self.user_collection: Collection = self.db[user_collection_name]
+        # self.household_collection: Collection = self.db[household_collection_name]
+
+    @property
+    def client(self) -> MongoClient:
+        return MongoClient(self.mongodb_url)
+    
+    @property
+    def db(self) -> Database:
+        return self.client[self.db_name]
+    
+    @property
+    def user_collection(self) -> Collection:
+        return self.db[self.user_collection_name]
+    
+    @property
+    def household_collection(self) -> Collection:
+        return self.db[self.household_collection_name]
