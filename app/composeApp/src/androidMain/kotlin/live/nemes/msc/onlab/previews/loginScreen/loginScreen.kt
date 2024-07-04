fd2d0c04c3f15795b05db1ad099010e5ad4c7e9e
Axel Roland Nemes <neaxro@gmail.com>
2024-07-04 16:24:37 +0200

    Update gitignore, added cache files.

---
+++ app/composeApp/src/androidMain/kotlin/live/nemes/msc/onlab/previews/loginScreen/loginScreen.kt
@@ -0,0 +1,11 @@
+package live.nemes.msc.onlab.loginScreen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ui.screens.loginScreen.loginScreen
+
+@Preview
+@Composable
+fun loginScreenPreview(){
+    loginScreen()
+}
